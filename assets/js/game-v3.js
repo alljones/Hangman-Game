b1c68762a362acd09a2d8ea54ca623032d09634e
Allan Jones <iamallanjones@gmail.com>
2018-02-16 01:31:19 -0600

    Structure and Random Picker

--- assets/js/game-v3.js
+++ assets/js/game-v3.js
@@ -12,8 +12,11 @@ var candyStore = ["skittles", "twizzlers", "snickers", "starburst", "butterfinge
 var candy = candyStore[Math.floor(Math.random() * candyStore.length)];
 var candyShown = document.getElementById('missing');
 
-//Hidden Word  
+//Hidden Word - Blanks  
 var blankCandy = [];
+
+//User Key Input  
+var userPick = [];
 
 //Number of Wins  
 var wins = 0;
@@ -23,7 +26,9 @@ var winsShown = document.getElementById('wins');
 var chances = 15;
 var chancesShown = document.getElementById('chances');
 
-//Letters Already Picked & displayed here  
+
+//Letters Already Picked & displayed here
+var wrongLetter = [];  
 var lettersShown = document.getElementById('lettersShown');
 
 
@@ -37,10 +42,32 @@ function startGame(){
 		blankCandy.push('_');
 		// console.log(blankCandy)
 	}
-	candyShown.textContent = blankCandy;
+	//Shows the Missing Blanks on Screen/Join Removes ',' and places a space in between '_'
+	candyShown.textContent = blankCandy.join(" ");
 
+	//RESET & Display on Screen
+	wrongLetter = [];
+	chances = 15;
+	chancesShown.textContent = chances;
+}
+//User Guesses
+document.onkeyup = function(event) {
+userPick = event.key;
+//console.log(userPick);
+
+//Checking to see if the letter is inside the word
+if(candy.indexOf(userPick) > -1){
+	for(var j = 0; i < candy.length; j++){
+		bla
+	}
+}
+else {
+	wrongLetter.push(userPick); //Store the Letter Choices
+	chances--; // Subtract Chances Tally
+		
 }
 
+}
 
 
 
@@ -48,4 +75,4 @@ function startGame(){
 CALL
 ******************/
 // First - Begin the Game by Any keypress
-document.onkeyup = startGame;
+//startGame();
